d5587ed6bbe8defca5d9c7b509a9da82b2405113
Noah Kovacs <kovacsn66@gmail.com>
2020-01-22 22:07:29 -0500

    feat(app, todo): initial setup, add todo template and show list w/o priority
    Signed-off-by: Noah Kovacs <kovacsn66@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'IBM-Challenge';
+  placeholderTodos = [
+    {
+      title: 'test',
+      priority: 1,
+    },
+    {
+      title: 'test 2',
+      priority: 2,
+    },
+    {
+      title: 'test 3',
+      priority: 3,
+    },
+  ];
 }
